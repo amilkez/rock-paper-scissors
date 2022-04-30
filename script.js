8ab16532870c1d1f9142ca088abd8e790d872102
angel <angelhidalgomb@gmail.com>
2022-04-30 10:33:39 -0500

    Remove old function helpers

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-//create computer play
-//returns 'rock', 'paper', or 'scissors' randomly
-
-function winGame(playerSel, computerSel) {
-	return `You win! ${playerSel} beats ${computerSel}`;
-}
-function loseGame(playerSel, computerSel) {
-	return `You lose! ${computerSel} beats ${playerSel}`;
-}
-function draw() {
-	return "No one wins, play again!";
-}
-
 function computerPlay() {
 	const randomNum = Math.ceil(Math.random() * 3);
 	return randomNum === 1 ? "Rock" : randomNum === 2 ? "Paper" : "Scissors";
@@ -65,5 +52,3 @@ function game() {
 		return "Better luck next time!";
 	}
 }
-
-console.log(game());
