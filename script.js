80b5a616b3dc7c2a75f16ca24d054582e3f42153
angel <angelhidalgomb@gmail.com>
2022-04-30 06:15:48 -0500

    Add case when user inputs rock

--- script.js
+++ script.js
@@ -15,3 +15,23 @@ function computerPlay() {
 	const randomNum = Math.ceil(Math.random() * 3);
 	return randomNum === 1 ? "Rock" : randomNum === 2 ? "Paper" : "Scissors";
 }
+
+function playRound(playerSelection, computerSelection) {
+	playerSelection = playerSelection.toLowerCase();
+	computerSelection = computerSelection.toLowerCase();
+
+	if (playerSelection === "rock") {
+		if (computerSelection === "rock") {
+			return draw();
+		} else if (computerSelection === "paper") {
+			return loseGame(playerSelection, computerSelection);
+		} else if (computerSelection === "scissors") {
+			return winGame(playerSelection, computerSelection);
+		}
+	}
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+
+console.log(playRound(playerSelection, computerSelection));
